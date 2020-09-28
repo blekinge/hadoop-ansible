cef3e403ba38f8dfeb4ed701078857a8faa9a5f1
Asger Askov Blekinge <abr@kb.dk>
2020-09-28 16:45:55 +0200

    Config for kerberos for HFDS

--- roles/zookeeper/templates/zookeeper-env.sh
+++ roles/zookeeper/templates/zookeeper-env.sh
@@ -8,5 +8,5 @@ export SERVER_JVMFLAGS=-Xmx1024m
 export JAVA=$JAVA_HOME/bin/java
 export CLASSPATH="$CLASSPATH:/usr/share/zookeeper/*"
 
-#export SERVER_JVMFLAGS="$SERVER_JVMFLAGS -Djava.security.auth.login.config=/usr/hdp/current/zookeeper-client/conf/zookeeper_jaas.conf"
-#export CLIENT_JVMFLAGS="$CLIENT_JVMFLAGS -Djava.security.auth.login.config=/usr/hdp/current/zookeeper-client/conf/zookeeper_client_jaas.conf"
+export SERVER_JVMFLAGS="$SERVER_JVMFLAGS -Djava.security.auth.login.config=/etc/zookeeper/conf/zookeeper_client_jaas.conf"
+export CLIENT_JVMFLAGS="$CLIENT_JVMFLAGS -Djava.security.auth.login.config=/etc/zookeeper/conf/zookeeper_client_jaas.conf"
