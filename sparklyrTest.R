b889dbb75e45543d4aef7e4d985614e91a6e1e52
Asger Askov Blekinge <abr@kb.dk>
2020-10-20 16:23:30 +0200

    Sparklyr works

---
+++ sparklyrTest.R
@@ -0,0 +1,21 @@
+library(sparklyr)
+
+# get the default config
+conf <- spark_config()
+
+conf$spark.authenticate.secret <- "test123"
+sc <- spark_connect(master = "yarn", config = conf)
+
+
+
+install.packages(c("nycflights13", "Lahman"))
+
+library(dplyr)
+iris_tbl <- copy_to(sc, iris)
+flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
+batting_tbl <- copy_to(sc, Lahman::Batting, "batting")
+src_tbls(sc)
+
+
+flights_tbl %>% filter(dep_delay == 2)
+
