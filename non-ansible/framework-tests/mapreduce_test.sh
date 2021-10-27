e9103e7ce46e93e2dcea2172084551acab850131
Asger Askov Blekinge <abr@kb.dk>
2021-10-27 14:38:50 +0200

    Mapreduce tests elaborated

--- non-ansible/framework-tests/mapreduce_test.sh
+++ non-ansible/framework-tests/mapreduce_test.sh
@@ -1,3 +1,16 @@
 hdfs dfs -mkdir input
 hdfs dfs -put /usr/local/hadoop/current/LICENSE.txt input/
-yarn jar /usr/local/hadoop/current/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar wordcount /user/$USER/input /user/$USER/output
+
+#This runs as an uber-task and thus we need a bit more memory than configured per default
+yarn jar /usr/local/hadoop/current/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar multifilewc \
+-Dyarn.app.mapreduce.am.command-opts="-Xmx2560m" \
+-Dyarn.app.mapreduce.am.resource.mb="3072" \
+/user/$USER/input /user/$USER/output
+
+#Print and delete results
+hdfs dfs -cat output/part-r-00000 | head
+hdfs dfs -rm -r output/
+
+
+# Calc PI
+yarn jar /usr/local/hadoop/current/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar pi 10 1000
